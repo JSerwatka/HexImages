d9649c1d1a04264c70150fb0a9ebdebfb673c3f5
Jakub Serwatka <kubaserw@gmail.com>
2021-10-04 18:47:21 +0200

    refactor: move new img generation to utils

--- thumbnailer/utils.py
+++ thumbnailer/utils.py
@@ -1,3 +1,5 @@
+import io 
+
 import PIL.Image
 
 
@@ -11,3 +13,21 @@ def resize_img_to_height(img, height):
 
     # Resize image and save in a file object
     return img.resize((wsize, height), PIL.Image.LANCZOS)
+
+def generate_new_img(original_img, original_requested, requested_height):
+    '''#TODO docstring'''
+
+    # Get the img
+    output = io.BytesIO()
+    img = PIL.Image.open(original_img.image.path) #TODO change path to url
+    # Check format - JPG or PNG
+    format = img.format
+
+    # Resize if needed
+    if not original_requested:
+        img = resize_img_to_height(img, requested_height)
+
+    img.save(output, format)
+    output.seek(0)
+
+    return output, format

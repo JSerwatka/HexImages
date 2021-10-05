d4c641dcf8560f0dabc136c93a1de17763eb9340
Jakub Serwatka <kubaserw@gmail.com>
2021-10-05 18:08:36 +0200

    refactor: remove slash before url path

--- thumbnailer/urls.py
+++ thumbnailer/urls.py
@@ -7,5 +7,5 @@ app_name = 'thumbnailer'
 
 urlpatterns = [
     path('', ResizeImg.as_view(), name='resize_img'),
-    path('/expiring-link/<str:uuid>', GetExpiringLink.as_view(), name='get_expiring_link')
+    path('expiring-link/<str:uuid>', GetExpiringLink.as_view(), name='get_expiring_link')
 ]
